06bf97657dd37f45cc5f511b84ff103881436f38
raghu <raghu160707@gmail.com>
2025-08-08 04:53:46 +0530

    ws gloabl template proto

--- websockets_again/src/index.ts
+++ websockets_again/src/index.ts
@@ -1,29 +1,70 @@
+// import express from "express";
+// import http from "http";
+// import { Event, WebSocketEventMap, WebSocketServer } from "ws";
+// const app = express();
+
+// const server = http.createServer(app);
+
+// const wss = new WebSocketServer({ server: server });
+
+// // function sendMessage(ws:any) {
+// //     let count = 0;
+// //     const interval = setInterval(() => {
+// //         if (ws.readyState === ws.OPEN) {
+// //             ws.send(`${count}`);
+// //             count++;
+// //         } else {
+// //             clearInterval(interval);
+// //         }
+// //     }, 1000);
+// // }
+
+
+
+// wss.on("connection",(ws)=>{
+    
+//     ws.send(wss.clients.size)
+
+//     ws.on('close',()=>{
+//         console.log(wss.clients.size);
+//     })
+// })
+
+
+// server.listen(8000, () => {
+//   console.log("server listeing on port 8000");
+// });
+
+
 import express from "express";
 import http from "http";
-import { matchesGlob } from "path";
 import { WebSocketServer } from "ws";
 
 const app = express();
-
 const server = http.createServer(app);
 
-const wss = new WebSocketServer({server:server})
+const wss = new WebSocketServer({ server });
 
-wss.on("connection",(ws)=>{
-    console.log("client connected")
-    ws.send("hi");
-    ws.addEventListener("message",(message)=>{
-        console.log(message.data)
-    })  
-    ws.addEventListener('close',(message)=>{
-        console.log(message.reason)
-    })
-    setTimeout(()=>{
-        ws.send("sending after 2 seconds")
-    },1000)
-})
+// Broadcast helper
+function broadcastCount() {
+    const count = wss.clients.size;
+    for (const client of wss.clients) {
+        if (client.readyState === client.OPEN) {
+            client.send(JSON.stringify({ count }));
+        }
+    }
+}
 
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+    broadcastCount(); 
 
-server.listen(8000,()=>{
-    console.log("server listening on port 8000")
-})
+    ws.on("close", () => {
+        console.log("Client disconnected");
+        broadcastCount(); 
+    });
+});
+
+server.listen(8000, () => {
+    console.log("Server listening on port 8000");
+});
