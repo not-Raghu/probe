a25dc98504027e4a6a425349a2c24f69f063a1bc
raghu <raghu160707@gmail.com>
2025-08-28 23:12:16 +0530

    more lesson

--- go/t4/main.go
+++ go/t4/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"os"
 )
 
 // func main() {
@@ -16,20 +17,40 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		args := os.Args
+		fmt.Print(args)
+		fmt.Print(args[0])
+		fmt.Print(args[1])
+		fmt.Print(args[2])
+		w.Write([]byte(args[0] + " " + args[1] + " " + args[2]))
+
+	})
+
 	mux.HandleFunc("/yes", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("yes route"))
 
 	})
 
 	mux.HandleFunc("/no", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("no route"))
+		w.Write([]byte("first no route"))
+		w.Write([]byte("second no route"))
 	})
 
 	mux.HandleFunc("/no/yes", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("no yes route"))
 	})
 
-	if http.ListenAndServe(":3000", mux) != nil {
-		log.Fatal("fatal error runing the server ")
-	}
+	go main2()
+	http.ListenAndServe(":3000", mux)
+
+}
+
+func main2() {
+	mux2 := http.NewServeMux()
+	mux2.HandleFunc("/mux2", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("this is mux 2 route"))
+	})
+	http.ListenAndServe(":3001", mux2)
 }
